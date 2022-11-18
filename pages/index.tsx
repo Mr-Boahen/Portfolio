91ba7fa646ef8fe90df8acd06928393bb2e73a34
Mr-Boahen <emmanuelnyantakyiboahen@gmail.com>
2022-11-18 14:14:43 +0000

    overflow-x-hidden

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll   z-0 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a] scrollbar-thin '>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden  z-0 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a] scrollbar-thin '>
       <Head>
         <title>Mr_Kaakyire</title>
        
